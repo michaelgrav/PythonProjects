445c0e811035cd31ef7ee5377a7d432eb8099f26
Michael Graves <mdgrav@protonmail.com>
2021-10-04 16:55:22 -0500

    Update main.py

--- LearningPython/main.py
+++ LearningPython/main.py
@@ -1,3 +1,4 @@
 # https://www.a1k0n.net/2011/07/20/donut-math.html
 # https://www.a1k0n.net/2021/01/13/optimizing-donut.html
 # https://www.youtube.com/watch?v=zn4Yvxww58g
+# https://www.youtube.com/watch?v=zEputlUuUjA
