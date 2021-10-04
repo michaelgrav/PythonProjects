49ee9c6f80f79bbaa6c797272539904e3f49b9e1
Michael Graves <mdgrav@protonmail.com>
2021-10-04 18:57:48 -0500

    Updated Python environment

--- LearningPython/main.py
+++ LearningPython/main.py
@@ -2,3 +2,6 @@
 # https://www.a1k0n.net/2021/01/13/optimizing-donut.html
 # https://www.youtube.com/watch?v=zn4Yvxww58g
 # https://www.youtube.com/watch?v=zEputlUuUjA
+
+import pygame as pg
+import numpy as np
