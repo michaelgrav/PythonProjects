0ec92d307e72dec3c87e020a99af1a42c54e9710
Michael Graves <mdgrav@protonmail.com>
2021-10-04 22:16:27 -0500

    Update ospathutils_start.py

--- LearningPython/Exercise Files/Ch4/ospathutils_start.py
+++ LearningPython/Exercise Files/Ch4/ospathutils_start.py
@@ -9,20 +9,25 @@ import time
 
 
 def main():
-  # Print the name of the OS
+    # Print the name of the OS
+    print(os.name)
 
-  
-  # Check for item existence and type
+    # Check for item existence and type
+    # print("Item exists: " + str(path.exists("textfile.txt")))
+    # print("Item is a file: " + str(path.isfile("textfile.txt")))
+    # print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
-  
-  # Work with file paths
+    # Work with file paths
+    # print("Item path: " + str(path.realpath("textfile.txt")))
+    # print("Item path and name: " + str(path.split(path.realpath("textfile.txt"))))
 
-  
-  # Get the modification time
+    # Get the modification time
+    t = time.ctime(path.getmtime("textfile.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
 
-  
-  # Calculate how long ago the item was modified
+    # Calculate how long ago the item was modified
 
-  
+
 if __name__ == "__main__":
-  main()
+    main()
